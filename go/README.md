f579a6b8f9405f1bd396825001ad4484b0482e18
Hugues Marandeau <h.marandeau@gmail.com>
2024-02-28 11:51:57 +0100

    Add dlv commands in README and handle request query params

--- go/README.md
+++ go/README.md
@@ -1,1 +1,17 @@
 Following https://gowebexamples.com tutorial
+
+```
+dlv debug hello.go
+list main.renderHomePage
+break ./hello.go:13
+
+(dlv) locals
+test_int_for_debug_dlv = 5
+test_debug = 4
+
+(dlv) args
+writer = net/http.ResponseWriter(*net/http.response) 0x14000157530
+request = ("*net/http.Request")(0x1400014ac60)
+
+print r.URL.Query().Get("token")
+```
--- go/hello.go
+++ go/hello.go
@@ -7,9 +7,16 @@ import (
 	"rsc.io/quote/v4"
 )
 
-func renderHomePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, you've requested %s\n", r.URL.Path)
-	fmt.Fprintf(w, "Fun quote: \t\"%s\" \n", quote.Go())
+func renderHomePage(writer http.ResponseWriter, request *http.Request) {
+	var test_int_for_debug_dlv int = 5
+	test_debug := 4
+	fmt.Printf("Test sum: %d + %d = %d\n", test_int_for_debug_dlv, test_debug, test_debug+test_int_for_debug_dlv)
+
+	// Accessing http://localhost/kekz/eze?param=testeze will print "testeze"
+	fmt.Printf("%s", request.URL.Query().Get("param"))
+
+	fmt.Fprintf(writer, "Hello, you've requested %s\n", request.URL.Path)
+	fmt.Fprintf(writer, "Fun quote: \t\"%s\" \n", quote.Go())
 
 }
 
