a03589007bb9d0cd66879b1cceec43d59dffa8e3
Hugues Marandeau <h.marandeau@gmail.com>
2024-02-28 11:58:26 +0100

    Add path to render static files

--- go/hello.go
+++ go/hello.go
@@ -8,9 +8,9 @@ import (
 )
 
 func renderHomePage(writer http.ResponseWriter, request *http.Request) {
-	var test_int_for_debug_dlv int = 5
-	test_debug := 4
-	fmt.Printf("Test sum: %d + %d = %d\n", test_int_for_debug_dlv, test_debug, test_debug+test_int_for_debug_dlv)
+	// var test_int_for_debug_dlv int = 5
+	// test_debug := 4
+	// fmt.Printf("Test sum: %d + %d = %d\n", test_int_for_debug_dlv, test_debug, test_debug+test_int_for_debug_dlv)
 
 	// Accessing http://localhost/kekz/eze?param=testeze will print "testeze"
 	fmt.Printf("%s", request.URL.Query().Get("param"))
@@ -20,8 +20,15 @@ func renderHomePage(writer http.ResponseWriter, request *http.Request) {
 
 }
 
+func handleRenderStatic() {
+	// http://localhost/static/pepelaugh.gif
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+}
+
 func startServer() {
 	http.HandleFunc("/", renderHomePage)
+	handleRenderStatic()
 
 	http.ListenAndServe(":80", nil)
 }
