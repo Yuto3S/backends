c315364c297b779d719a976bac708ebe33734642
Hugues Marandeau <h.marandeau@gmail.com>
2024-02-28 18:10:11 +0100

    Complete user deletion and iteration over all users with mysql matching model

--- go/example_mysql.go
+++ go/example_mysql.go
@@ -23,7 +23,10 @@ func initTables() {
 		createUserTable(db)
 	}
 
-	insertRandomUser(db)
+	userIdInserted := insertRandomUser(db)
+	selectUser(userIdInserted, db)
+	getAllUsers(db)
+	deleteUser(userIdInserted, db)
 }
 
 func usersTableExists(db *sql.DB) bool {
@@ -60,7 +63,7 @@ func dropUsersTable(db *sql.DB) {
 }
 
 // https://github.com/random-names/go
-func insertRandomUser(db *sql.DB) {
+func insertRandomUser(db *sql.DB) int64 {
 	username := "johndoe"
 	password := "secret"
 	createdAt := time.Now()
@@ -68,4 +71,68 @@ func insertRandomUser(db *sql.DB) {
 	result, _ := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
 	userID, _ := result.LastInsertId()
 	fmt.Println("USER ID INSERTED:", userID)
+	return userID
+}
+
+func selectUser(userId int64, db *sql.DB) {
+	var (
+		id        int
+		username  string
+		password  string
+		createdAt time.Time
+	)
+
+	query := `SELECT id, username, password, created_at FROM users WHERE id = ?`
+	err := db.QueryRow(query, userId).Scan(&id, &username, &password, &createdAt)
+	fmt.Println("User id ", userId, " has values: ", username, password, createdAt)
+	if err != nil {
+		fmt.Println("error select user ", err)
+	}
+}
+
+func getAllUsers(db *sql.DB) {
+	type user struct {
+		id        int
+		username  string
+		password  string
+		createdAt time.Time
+	}
+
+	rows, err := db.Query(`SELECT id, username, password, created_at FROM users`)
+	if err != nil {
+		fmt.Println("error get all users ", err)
+	}
+	defer rows.Close()
+
+	var users []user
+	for rows.Next() {
+		var u user
+		err := rows.Scan(&u.id, &u.username, &u.password, &u.createdAt)
+		if err != nil {
+			fmt.Println("error row scan ", err)
+		}
+		users = append(users, u)
+	}
+	errr := rows.Err()
+	if errr != nil {
+		fmt.Println("error in rows ", errr)
+	}
+
+	for index, user := range users {
+		fmt.Println(index, user.username, user.password, user.createdAt)
+	}
+}
+
+func deleteUser(userId int64, db *sql.DB) {
+	delete_user_query := `DELETE FROM users WHERE id = ?`
+	result, err := db.Exec(delete_user_query, userId)
+	if err != nil {
+		fmt.Println("Error deleting user ", err)
+
+	}
+
+	if result != nil {
+		rowsAffected, _ := result.RowsAffected()
+		fmt.Println("Number of user rows deleted: ", rowsAffected)
+	}
 }
