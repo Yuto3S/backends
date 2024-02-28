fe9f1202434d88370c5366bac74e53fcebfaab76
Hugues Marandeau <h.marandeau@gmail.com>
2024-02-28 11:23:35 +0100

    First part of HTTP Server

--- go/hello.go
+++ go/hello.go
@@ -3,18 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+
 	"rsc.io/quote/v4"
 )
 
+func renderHomePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, you've requested %s\n", r.URL.Path)
+	fmt.Fprintf(w, "Fun quote: \t\"%s\" \n", quote.Go())
+
+}
+
 func startServer() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { 
-		fmt.Fprintf(w, "Hello, you've requested %s\n", r.URL.Path)
-	})
+	http.HandleFunc("/", renderHomePage)
 
 	http.ListenAndServe(":80", nil)
 }
 
 func main() {
-	fmt.Println(quote.Go())
 	startServer()
 }
