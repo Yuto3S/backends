53cdc66a445ab4f9d7c55b5a062fbca4cad25474
Hugues Marandeau <h.marandeau@gmail.com>
2024-03-12 17:30:20 +0100

    Try to add maven and fix build that doesnt work

--- java/src/main/java/com/example/springboot/HelloController.java
+++ java/src/main/java/com/example/springboot/HelloController.java
@@ -1,5 +1,9 @@
 package com.example.springboot;
 
+// import java.io.File;
+// import java.io.FileNotFoundException;
+// import java.util.Scanner;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +17,20 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String helloPage() {
+        // String info = getInfoFromFile();
         return "Hit on the Hello Page";
     }
+
+    // private String getInfoFromFile() {
+    //     try {
+    //         File myFile = new File("myfile.json");
+    //         Scanner myReader = new Scanner(myFile);
+    //     } catch (FileNotFoundException e) {
+    //         System.out.println("Couldn't find file");
+    //         e.printStackTrace();
+    //     }
+
+    //     return "ok";
+    // }
     
 }
