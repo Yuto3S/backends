958d11f8139d7efd3e3b4b4d04d46f98f183bfea
Hugues Marandeau <h.marandeau@gmail.com>
2024-03-20 16:07:07 +0100

    bunch of manual changes

---
+++ kotlin/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,17 @@
+package main.kotlin.com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class MessageController {
+	@GetMapping("/")
+	fun index(@RequestParam("name") name: String) = "Hello, $name!"
+}
