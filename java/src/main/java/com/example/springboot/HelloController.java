08286633379060ad67f9720868eae8afab4ed730
Hugues Marandeau <h.marandeau@gmail.com>
2024-03-27 13:52:54 +0100

    Move springboot folder to java folder (renaming)

--- java/src/main/java/com/example/springboot/HelloController.java
+++ java/src/main/java/com/example/springboot/HelloController.java
@@ -1,9 +1,5 @@
 package com.example.springboot;
 
-// import java.io.File;
-// import java.io.FileNotFoundException;
-// import java.util.Scanner;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,25 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot";
+        return "Greetings from Spring Boot!";
     }
-
-    @GetMapping("/hello")
-    public String helloPage() {
-        // String info = getInfoFromFile();
-        return "Hit on the Hello Page";
-    }
-
-    // private String getInfoFromFile() {
-    //     try {
-    //         File myFile = new File("myfile.json");
-    //         Scanner myReader = new Scanner(myFile);
-    //     } catch (FileNotFoundException e) {
-    //         System.out.println("Couldn't find file");
-    //         e.printStackTrace();
-    //     }
-
-    //     return "ok";
-    // }
-    
 }
--- java/src/test/java/com/example/springboot/HelloControllerTest.java
+++ java/src/test/java/com/example/springboot/HelloControllerTest.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,17 +21,9 @@ public class HelloControllerTest {
     private MockMvc mvc;
 
     @Test
-    public void getRoot() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk())
-        .andExpect(content().string(equalTo("Greetings from Spring Boot")));
-    }
-
-    @Test
     public void getHello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk())
-        .andExpect(content().string(equalTo("Hit on the Hello Page")));
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
     }
-    
 }
